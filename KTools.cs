3e031ad5202c5992a5333d0aca7bceb762d88667
Kingfisher <longvt.dev@gmail.com>
2026-08-18 14:19:55 +0700

    [K-Setting] remove KReference in setting

--- KTools.cs
+++ KTools.cs
@@ -16,7 +16,7 @@ namespace Kingfisher.KSetting
 
         private const string MenuTypeNameFormat = "Kingfisher.{0}.{0}Menu, Kingfisher.{0}";
 
-        public static readonly string[] ToolNames = { "KFolders", "KHierarchy", "KInspector", "KFavorites", "KReference", "KTabs", "KEmoji" };
+        public static readonly string[] ToolNames = { "KFolders", "KHierarchy", "KInspector", "KFavorites", "KTabs", "KEmoji" };
 
         private static List<KTool> _installed;
 
